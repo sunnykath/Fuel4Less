66bb815639b2f92a3e3cfbbf17aec0f1834defec
sunnykath <48870337+sunnykath@users.noreply.github.com>
2021-05-04 18:45:12 +1200

    Initialise Repository with examples folder structure

---
+++ backend/src/routes/api/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const router = express.Router();
+
+// TODO: Implement Routing
+
+// import articles from './articles';
+// router.use('/articles', articles);
+
+// import images from './images';
+// router.use('/images', images);
+
+export default router;
