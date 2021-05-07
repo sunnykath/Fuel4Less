42218987281b89511792acbecfc04fab3a1ade41
sunnykath <48870337+sunnykath@users.noreply.github.com>
2021-05-07 18:10:19 +1200

    Add Login Form for Landing Page

---
+++ frontend/src/Components/forms/LoginForm.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import {Button, Input, TextField} from "@material-ui/core";
+import "./LoginForm.css"
+
+
+// import "./styles.css";
+// import CustomInput from "./components/CustomInput";
+// import Button from "./components/Button";
+
+// export default class LoginForm extends Component {
+
+function LoginForm(params) {
+
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // handleChange = e => {
+  //   this.setcredentials({ [e.currentTarget.id]: e.currentTarget.value });
+  // };
+
+    return (
+      <div>
+        <form className="form">
+          
+          <TextField
+            label="Email"
+            id="email"
+            // formControlProps={{
+            //   fullWidth: true
+            // }}
+            onChange={(e) => {setEmail(e.target.value)}}
+            type="text"
+            style={{margin: 10}}
+          />
+          <TextField
+            label="Password"
+            id="password"
+            // formControlProps={{
+            //   fullWidth: true
+            // }}
+            onChange={(e) => {setPassword(e.target.value)}}
+            type="password"
+            style={{margin: 10}}
+          />
+
+          <Button className="logInButton" variant="contained" color="secondary" disabled={(password && email) ? 0: 1} onClick={() => { console.log(email,password) }}/*className="form__custom-button"*/>
+            Log in
+          </Button>
+
+        </form>
+      </div>
+    );
+}
+
+
+export default LoginForm;
--- frontend/src/Pages/SignInPage.js
+++ frontend/src/Pages/SignInPage.js
@@ -1,15 +1,23 @@
+import React, { useState } from "react";
+import {Button} from "@material-ui/core"
+
 import NavBar from "../Components/NavBar";
+import LoginForm from "../Components/forms/LoginForm"
+
 
 export default function SignInPage() {
     
     return(
-        <div className="SignInPage">
+        // <div className="SignInPage">
+        <div>
             <nav>
                 <NavBar />
             </nav>
-            <p>
-                form.js
-            </p>
+            <h1> Fuel 4 Less</h1>
+            <LoginForm/>
+            <Button variant="contained" color="primary">
+                Sign up
+            </Button>
         </div>
         
     );
