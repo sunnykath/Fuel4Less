5076013cf9d28f8884f0ac7d636fc66512119697
Preetham-13 <pgar971@aucklanduni.ac.nz>
2021-05-07 22:07:12 +1200

    adding map functionality to the map page

--- frontend/src/Pages/MapsPage.js
+++ frontend/src/Pages/MapsPage.js
@@ -1,16 +1,52 @@
 import NavBar from "../Components/NavBar";
+import React, { Component, useState, useEffect } from 'react';
+import { Map, Marker, GoogleMap, withScriptjs, withGoogleMap, GoogleApiWrapper, HandleGoogleMap } from "google-maps-react";
 
-export default function MapsPage() {
+function MapsPage(props) { 
+   
+    const [ currentPosition, setCurrentPosition ] = useState({});
 
-    return(
+    const mapStyles = {
+        width: '100%',
+        height: '50%'
+      };
+
+    const success = position => {
+        const currentPosition = {
+          lat: position.coords.latitude, 
+          lng: position.coords.longitude
+        }
+        setCurrentPosition(currentPosition);
+    };
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(success);
+    })
+   
+    return (
         <div className="MapsPage">
             <nav>
-                <NavBar />
+              <NavBar />
             </nav>
-            <p>
-                 Map.png
-            </p>
-        </div>
-        
-    );
+            <Map
+            google={props.google}
+            zoom={12}
+            style={mapStyles}
+            center={
+                {
+                    lat: currentPosition.lat,
+                    lng: currentPosition.lng
+                }
+            }
+            />
+            <Marker
+            title={'The marker`s title will appear as a tooltip.'}
+            name={'SOMA'}
+            position={{lat: currentPosition.lat, lng: currentPosition.lng}} />
+      </div>
+    )
 }
+
+export default GoogleApiWrapper({
+    apiKey: "AIzaSyBpGBzv1hYJIzC4MQto84nI9CBPQIqxoCU"
+  })(MapsPage);
