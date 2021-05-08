0a32c6a9d7a2e3ba3722e63e0187443fb470ba0f
Preetham-13 <pgar971@aucklanduni.ac.nz>
2021-05-08 15:44:08 +1200

    get rid of unused code

--- frontend/src/Pages/MapsPage.js
+++ frontend/src/Pages/MapsPage.js
@@ -3,15 +3,11 @@ import MapComponent from "../Components/MapComponent";
 
 export default function MapPage() {
   return(
-    // <div className="SignInPage">
     <div className="MapsPage">
         <nav>
             <NavBar />
         </nav>
         <MapComponent/>
-        {/* <Button className="button" variant="contained" color="primary">
-            Sign up as an Admin
-        </Button> */}
     </div>
     
 );
